fdd70a61ce40e2df29382c4bf0bc3fbb7d891d44
ppoptart12 <125997744+ppoptart12@users.noreply.github.com>
2024-08-17 22:54:02 +0800

    Implemented subject line into main extension page and onto drafted emails

--- gmail_copilot/scripts/Copilot_functions.js
+++ gmail_copilot/scripts/Copilot_functions.js
@@ -12,17 +12,19 @@ document.getElementById('runButton').addEventListener('click', () => {
     
     .then(response => response.json())
     .then(data => {
-        final_body = data.replace(/\n/g, "<br>").replace(/\n\n/g, "<br>").replaceAll("^\"|\"$", "");
+        final_email_subject = data.email_subject.replace(/\n/g, "<br>").replace(/\n\n/g, "<br>").replaceAll("^\"|\"$", "");
+        final_email_body = data.email_body.replace(/\n/g, "<br>").replace(/\n\n/g, "<br>").replaceAll("^\"|\"$", "");
 
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             // Check if the content script has already been injected
             chrome.scripting.executeScript({
                 target: { tabId: tabs[0].id },
                 function: injectIfNotExists,
-                args: [final_body]  
+                args: [final_email_subject, final_email_body]  
             });
         });
-        document.getElementById('output').innerHTML = final_body;
+        document.getElementById('SubjectOutput').innerHTML = final_email_subject;
+        document.getElementById('BodyOutput').innerHTML = final_email_body;
     })
     .catch(error => {
         console.error('Error:', error);
@@ -30,33 +32,37 @@ document.getElementById('runButton').addEventListener('click', () => {
     });
 });
 
-function injectIfNotExists(finalBody) {
+function injectIfNotExists(finalBody, finalSubject) {
     if (!window.hasInjectedCopilotScript) {
         chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             if (request.action === "composeEmail") {
-                startComposeEmail(request.text);
+                startComposeEmail(request.subject, request.text);
             }
         });
         window.hasInjectedCopilotScript = true;
     }
 
-    startComposeEmail(finalBody);
+    startComposeEmail(finalBody, finalSubject);
 }
-
-function copyText() {
-    var textToCopy = document.getElementById("output").innerHTML.replace(/<br\s*\/?>/gi, '\n');
+function copyText(elementId, buttonId) {
+    var textToCopy = document.getElementById(elementId).innerHTML.replace(/<br\s*\/?>/gi, '\n');
     
-
-    var buttonImg = document.getElementById("ButtonImage");
+    var buttonImg = document.getElementById(buttonId);
     buttonImg.src = "../images/CheckMark.png"; 
 
     setTimeout(function() {
             buttonImg.src = "../images/CopyText.png";
         }, 1000);
             
-    navigator.clipboard.writeText(textToCopy)
+    navigator.clipboard.writeText(textToCopy);
 }
 
-document.getElementById('CopyButton').addEventListener('click', () => {
-    copyText()
+// First copy button
+document.getElementById('CopyButton1').addEventListener('click', () => {
+    copyText('SubjectOutput', 'ButtonImage1');
+});
+
+// Second copy button
+document.getElementById('CopyButton2').addEventListener('click', () => {
+    copyText('BodyOutput', 'ButtonImage2');
 });
